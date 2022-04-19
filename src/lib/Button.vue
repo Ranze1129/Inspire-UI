1ae3c175d2ffca4f6122c8eaaf94a44155d8ad4f
Ranze <1066467243@qq.com>
2022-04-19 18:15:25 +0800

    支持 level 属性

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -17,14 +17,19 @@ export default {
     size: {
       type: String,
       default: 'normal'
-    }
+    },
+    level: {
+      type: String,
+      default: "normal",
+    },
   },
   setup(props) {
-    const {theme, size} = props;
+    const {theme, size, level} = props;
     const classes = computed(() => {
       return {
         [`inspire-theme-${theme}`]: theme,
         [`inspire-size-${size}`]: size,
+        [`inspire-level-${level}`]: level,
       };
     });
     return {classes};
@@ -38,6 +43,7 @@ $border-color: #d9d9d9;
 $color: #333;
 $blue: #40a9ff;
 $radius: 4px;
+$red: red;
 .inspire-button {
   box-sizing: border-box;
   height: $h;
@@ -52,6 +58,7 @@ $radius: 4px;
   border: 1px solid $border-color;
   border-radius: $radius;
   box-shadow: 0 1px 0 fade-out(black, 0.95);
+  transition: background 250ms;
   & + & {
     margin-left: 8px;
   }
@@ -82,15 +89,62 @@ $radius: 4px;
       background: darken(white, 5%);;
     }
   }
-    &.inspire-size-big {
-      font-size: 24px;
-      height: 48px;
-      padding: 0 16px
+  &.inspire-size-big {
+    font-size: 24px;
+    height: 48px;
+    padding: 0 16px
+  }
+  &.inspire-size-small {
+    font-size: 12px;
+    height: 20px;
+    padding: 0 4px;
+  }
+  &.inspire-theme-button {
+    &.inspire-level-main {
+      background: $blue;
+      color: white;
+      border-color: $blue;
+      &:hover,
+      &:focus {
+        background: darken($blue, 10%);
+        border-color: darken($blue, 10%);
+      }
     }
-    &.inspire-size-small {
-      font-size: 12px;
-      height: 20px;
-      padding: 0 4px;
+    &.inspire-level-danger {
+      background: $red;
+      border-color: $red;
+      color: white;
+      &:hover,
+      &:focus {
+        background: darken($red, 10%);
+        border-color: darken($red, 10%);
+      }
     }
+  }
+  &.inspire-theme-link {
+    &.inspire-level-danger {
+      color: $red;
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
+  }
+  &.inspire-theme-text {
+    &.inspire-level-main {
+      color: $blue;
+      &:hover,
+      &:focus {
+        color: darken($blue, 10%);
+      }
+    }
+    &.inspire-level-danger {
+      color: $red;
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
+  }
 }
 </style>
