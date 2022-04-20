a35103f964b57317bb18b1cd382b20294966486a
Ranze <1066467243@qq.com>
2022-04-20 14:44:28 +0800

    创建 Dialog 组件

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,5 +1,14 @@
 <template>
   <div>
-    DialogDemo
+    <hi>示例1</hi>
+    <Dialog />
   </div>
 </template>
+
+<script lang="ts">
+import Dialog from '../lib/Dialog.vue'
+export default {
+  components: {Dialog}
+}
+
+</script>
