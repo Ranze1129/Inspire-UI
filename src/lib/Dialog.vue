1b13ced78fb4b8c62ea4b8f896483e7d7b8e4e43
Ranze <1066467243@qq.com>
2022-04-20 14:55:53 +0800

    Dialog css 1

--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -1,15 +1,17 @@
 <template>
-  <div class="dialog-overlay"></div>
-  <div class="dialog-wrapper">
-    <header>标题</header>
-    <main>
-      <p>第一行字</p>
-      <p>第二行字</p>
-    </main>
-    <footer>
-      <Button>OK</Button>
-      <Button>Cancel</Button>
-    </footer>
+  <div class="inspire-dialog-overlay"></div>
+  <div class="inspire-dialog-wrapper">
+    <div class="inspire-dialog">
+      <header>标题 <span class="inspire-dialog-close"></span></header>
+      <main>
+        <p>第一行字</p>
+        <p>第二行字</p>
+      </main>
+      <footer>
+        <Button level="main">确 定</Button>
+        <Button>取 消</Button>
+      </footer>
+    </div>
   </div>
 </template>
 
@@ -18,6 +20,69 @@ import Button from './Button.vue';
 export default {
   components: {Button}
 };
-
-
 </script>
+
+<style lang="scss">
+$radius: 4px;
+$border-color: #d9d9d9;
+.inspire-dialog {
+  background: white;
+  border-radius: $radius;
+  box-shadow: 0 0 3px fade_out(black, 0.5);
+  min-width: 18em;
+  max-width: 90%;
+  &-overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: fade_out(black, 0.5);
+    z-index: 10;
+  }
+  &-wrapper {
+    position: fixed;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 11;
+  }
+  >header {
+    padding:  12px 16px 12px 18px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    font-size: 20px;
+  }
+  >main {
+    padding:  12px 16px 12px 18px;
+  }
+  >footer {
+    padding:  12px 16px 12px 18px;
+    text-align: right;
+  }
+  &-close {
+    position: relative;
+    display: inline-block;
+    width: 16px;
+    height: 16px;
+    cursor: pointer;
+    &::before,
+    &::after {
+      content: '';
+      position: absolute;
+      height: 1px;
+      background: black;
+      width: 100%;
+      top: 50%;
+      left: 50%;
+    }
+    &::before {
+      transform: translate(-50%, -50%) rotate(-45deg);
+    }
+    &::after {
+      transform: translate(-50%, -50%) rotate(45deg);
+    }
+  }
+}
+</style>
