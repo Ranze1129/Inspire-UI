bebd9335d21670f51cc439671d3f37bb30526ccc
Ranze <1066467243@qq.com>
2022-04-23 16:47:03 +0800

    解决生产环境md加载问题

--- src/router.ts
+++ src/router.ts
@@ -1,29 +1,34 @@
 import {createRouter, createWebHashHistory} from 'vue-router';
-import Home from './views/Home.vue'
-import Doc from './views/Doc.vue'
-import SwitchDemo from './components/SwitchDemo.vue'
-import ButtonDemo from './components/ButtonDemo.vue'
-import DialogDemo from './components/DialogDemo.vue'
-import TabsDemo from './components/TabsDemo.vue'
-import Intro from './views/Intro.vue'
-import {h} from 'vue'
+import Home from './views/Home.vue';
+import Doc from './views/Doc.vue';
+import SwitchDemo from './components/SwitchDemo.vue';
+import ButtonDemo from './components/ButtonDemo.vue';
+import DialogDemo from './components/DialogDemo.vue';
+import TabsDemo from './components/TabsDemo.vue';
+import Intro from './views/Intro.vue';
+import {h} from 'vue';
 import Markdown from './components/Markdown.vue';
+// @ts-ignore
+import getStarted from './markdown/get-started.md';
+// @ts-ignore
+import install from './markdown/install.md';
 
-const history = createWebHashHistory()
-const md = filename => h(Markdown, { path: `../markdown/${filename}.md`, key: filename })
+const history = createWebHashHistory();
+const md = string => h(Markdown, {content: string, key: string});
 export const router = createRouter({
   history: history,
-  routes:[
+  routes: [
     {path: '/', component: Home},
-    {path: '/doc' , component: Doc, children:[
-        {path:'', redirect: '/doc/intro'},
-        {path:'intro', component: Intro},
-        { path: "get-started", component: md('get-started') },
-        { path: "install", component: md('install') },
-        {path:'switch', component: SwitchDemo},
-        {path:'button', component: ButtonDemo},
-        {path:'dialog', component: DialogDemo},
-        {path:'tabs', component: TabsDemo}
+    {
+      path: '/doc', component: Doc, children: [
+        {path: '', redirect: '/doc/intro'},
+        {path: 'intro', component: Intro},
+        {path: 'get-started', component: md(getStarted)},
+        {path: 'install', component: md(install)},
+        {path: 'switch', component: SwitchDemo},
+        {path: 'button', component: ButtonDemo},
+        {path: 'dialog', component: DialogDemo},
+        {path: 'tabs', component: TabsDemo}
       ]}
   ]
 })
